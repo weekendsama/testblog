e096994df8ef4554242739be6e25626b54bf37db
weekendsama <1317555846@qq.com>
2020-06-29 00:28:07 +0800

    Initial commit

---
+++ blog/feed.py
@@ -0,0 +1,18 @@
+from django.contrib.syndication.views import Feed
+
+from .models import Post
+
+
+class AllPostsFeed(Feed):
+    title = 'HelloDjango-blog-tutorial'
+    link = '/'
+    description = 'HelloDjango-blog-tutorial 全部文章'
+
+    def items(self):
+        return Post.objects.all()
+
+    def item_title(self, item):
+        return '[%s] %s' % (item.category, item.title)
+
+    def item_description(self, item):
+        return item.body_html
---
+++ blog/views.py
@@ -0,0 +1,90 @@
+import markdown
+import re
+from pure_pagination import PaginationMixin
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.text import slugify
+from django.db.models import Q
+from django.contrib import messages
+from markdown.extensions.toc import TocExtension
+from .models import Post, Category, Tag
+from django.views.generic import ListView, DetailView
+# Create your views here.
+
+
+class IndexView(ListView, PaginationMixin):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    paginate_by = 5
+
+
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super().get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        # post.toc = m.group(1) if m else ''
+
+        return post
+
+
+class ArchiveView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchiveView, self).get_queryset().filter(
+                                    created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class TagView(IndexView):
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=t).order_by('-created_time')
+
+
+class SearchView(ListView):
+    def get_queryset(self):
+        q = self.Get.get
+def search(request):
+    q = request.GET.get('q')
+    if not q:
+        error_msg = '请输入搜索关键词'
+        messages.add_message(request, messages.ERROR, error_msg, extra_tags='danger')
+        return redirect('blog:index')
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'post_list': post_list})
