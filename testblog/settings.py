67cc816249fc149389def203d5754abbcda31ce9
weekendsama <1317555846@qq.com>
2020-07-06 20:31:20 +0800

    中文化后台

--- testblog/settings.py
+++ testblog/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC-8'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
